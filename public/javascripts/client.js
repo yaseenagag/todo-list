ff68f116ea0217b09b7cd3b623add12f5600aa09
sdweber422 <sdweber422@gmail.com>
2016-11-15 17:18:25 -0800

    Fix login, implement session, user now persists, add client.js to public/javascripts

---
+++ public/javascripts/client.js
@@ -0,0 +1,20 @@
+
+$(document).ready( function() {
+  $('.task input[type=checkbox]').change( function() {
+    var url = '/tasks/' +
+      ( $(this).is( ':checked' ) ? 'complete' : 'uncomplete' ) +
+      'Task'
+    var id = $(this).data( 'id' )
+
+    $.post( url, { id: id } )
+  })
+
+  $('input.task-description').on( 'keypress', function(event) {
+    if( event.charCode === 13 ) {
+      var id = $(this).data( 'id' )
+      var value = $(this).val()
+
+      $.post( '/tasks/update', { id: id, value: value } )
+    }
+  })
+})
